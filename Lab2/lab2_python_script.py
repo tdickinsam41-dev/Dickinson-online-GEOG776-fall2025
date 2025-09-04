28f04c8c552e38ec2673e52e2283fe0fd4e4be0a
tdickinsam41-dev <tdickinsam41@tamu.edu>
2025-09-03 22:38:53 -0500

    complete initial script for lab2

--- Lab2/lab2_python_script.py
+++ Lab2/lab2_python_script.py
@@ -6,3 +6,22 @@ for i in part1:
     result1 = result1 * i
 
 print ('The product of all members of the part1 list is: ', result1)
+
+# question 2
+part2 = [-1, 23, 483, 8573, -13847, -381569, 1652337, 718522177]
+result2 = 0 # initialize
+
+for i in part2:
+    result2 = result2 + i
+
+print ('The sum of all members of the part2 list is: ', result2)
+
+# question 3
+part3 = [146, 875, 911, 83, 81, 439, 44, 5, 46, 76, 61, 68, 1, 14, 38, 26, 21] 
+result3 = 0 # initialize
+
+for i in part3:
+    if i % 2 == 0:
+        result3 = result3 + i
+
+print ('The sum of all even members of the part3 list is: ', result3)       
