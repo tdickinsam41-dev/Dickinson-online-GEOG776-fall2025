1593ff1a14a282298a61f2b6280da6c353834027
tdickinsam41-dev <tdickinsam41@tamu.edu>
2025-10-15 22:48:40 -0500

    Initial Lab7

--- Lab7/README.md
+++ Lab7/README.md
@@ -1,2 +1,16 @@
-# Dickinson-online-GEOG776-fall2025
-geog 776 repo
+# Dickinson-Online-GEOG676-Fall2025
+## GEOG 676 Lab 7
+
+### Lab 7 requires the following tasks to be completed:
+
+1. Find and download satellite imagery that is split up by band. Landsat is a great choice. Add the imagery to your Github.
+1. Using the satellite imagery you downloaded, create a composite raster image
+1. Find a digital elevation model of your area of interest and create the following:
+
+    2. A hillshade analysis raster
+    2. A slope analysis raster 
+
+
+#### The following screen capture shows the map from the new project created from the new graduatedcolor tool:
+
+![Window snap](lab6_screen_output.jpg)
---
+++ Lab7/lab7_script.py
@@ -0,0 +1,28 @@
+import arcpy
+
+## set source and target folders
+source = r"C:\A&M\GEOG676\Dickinson-online-GEOG776-fall2025\Lab7\images"
+target = r"C:\A&M\GEOG676\Dickinson-online-GEOG776-fall2025\Lab7"
+
+## assign bands
+band1 = arcpy.sa.Raster(source + r"\band1.tif")
+band2 = arcpy.sa.Raster(source + r"\band2.tif")
+band3 = arcpy.sa.Raster(source + r"\band3.tif")
+band4 = arcpy.sa.Raster(source + r"\band4.tif")
+
+## create composite
+composite = arcpy.CompositeBands_management([band1, band2, band3, band4], target + r"\composite.tif")
+
+## create hillshade
+azimuth = 315
+altitude = 45
+shadows = "No_Shadows"
+z_factor = 1
+arcpy.ddd.HillShade(source + f"\DEM.tif", target + r"\output_hillshade.tif", azimuth, altitude, shadows, z_factor)
+
+## create slope
+output_measurement = "DEGREE"
+z_factor = 1
+arcpy.ddd.Slope(source + r"\DEM.tif", target + r"\output_slope.tif", output_measurement, z_factor)
+
+print("success")
